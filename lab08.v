69e3c6fcdb1b881d8e8cd98a3cc6c6764574e75b
Brandon Garrido <gar19421@uvg.edu.gt>
2020-10-24 20:17:53 -0600

    Initial commit

---
+++ lab08.v
@@ -0,0 +1,54 @@
+// programa 1
+module Counter(input wire clk, reset, enable, load,  output reg[11:0] count);
+//contador de 12 bits con entradas de clock, reset, enable y load
+
+  //conteo en cada flanco de reloj
+  always @ (posedge clk or posedge reset or posedge enable or posedge load) begin
+    if(reset) begin
+     count <= 12'd0; // si se resetea empieza en cero
+    end
+    else if(clk & enable & ~load) begin
+        #2 count <= count + 12'd1; // si esta en enable cuenta el contador sino no cuenta
+    end
+    else if(clk & load & enable) begin
+        #2 count <= 12'd34; //precargando que empiece en 34 base 10
+    end
+  end
+
+endmodule
+
+//programa 2
+module Memory (input wire [11:0] address, output wire [7:0] word);//memoria rom
+  reg [7:0] mem [0:4095];// creacion de arreglo de memoria
+  initial begin
+      $readmemb("memory.list", mem);//lectura del archivo de datos
+  end
+  assign word = mem[address];// resultado leido de la memoria
+endmodule
+
+// programa 3
+module ALU(input wire [3:0] A, B, input wire [2:0] F, output reg [3:0] Resultado);//implementacion de la ALU
+
+// parametros tabla 5.1 libro
+  parameter AND1 = 3'd0;
+  parameter OR1 = 3'd1;
+  parameter ADD = 3'd2;
+  parameter AND2 = 3'd4;
+  parameter OR2 = 3'd5;
+  parameter SUBTRACT = 3'd6;
+  parameter SLT = 3'd7;
+
+  always @(*) begin// operaciones a realizar con la ALU con un switch case
+    case(F)
+    AND1:  assign Resultado = (A & B);
+    OR1:   assign Resultado = (A | B);
+    ADD:  assign Resultado = A + B;
+    AND2: assign Resultado = (A & ~B);
+    OR2:  assign Resultado = (A | ~B);
+    SUBTRACT: assign Resultado = A - B;
+    SLT: assign Resultado = (A > B) ? 4'd0 : 4'd1;
+    default: Resultado = 4'd0;
+    endcase
+  end
+
+endmodule
---
+++ lab08_tb.v
@@ -0,0 +1,82 @@
+module testbench();
+reg clock, reset, enable, load;//variables para el contador
+wire [11:0] salida;
+
+wire [7:0]word;
+reg  [11:0]address;//variables para la memoria
+integer i;
+
+reg [2:0] F;
+reg [3:0] A, B;
+wire [3:0] salida_2;//variables para el ALU
+
+always//instanciacion del clock
+  begin
+    clock <= 1;
+    #1 clock <= ~clock;// se realiza el cambio del reloj
+    #1;
+end
+
+Counter Contador(clock, reset, enable, load, salida);
+Memory  Memoria(address, word);
+ALU     ALU1(A, B, F, salida_2);// instanciacion de los modulos de cada ejercicio
+
+initial begin// test de contador
+  $display("Contador\n");
+  $display("CLK Reset | Enable Load | Contador");
+  $display("----------|-------------|--------------");
+  $monitor("%b     %b   |    %b     %b  | %b", clock,reset,enable,load,salida);
+  #2 reset = 1;
+  #2 enable = 0; load = 0; reset = 0;
+  #2; #2;
+  #2 enable = 1; load = 0;
+  #2; #2; #2; #2;
+  #2 load = 1;
+  #2 load = 0 ;
+  #2; #2; #2;
+  #2 enable = 0;
+  #2; #2; #2;
+
+end
+
+initial begin//test de memoria
+  #38
+  $display(" Memoria ROM ");
+  $display("Address       |    Word   ");
+  $display("--------------|-------------");
+  $monitor("%b  |   %b", address, word);
+
+  for (i = 0; i < 10; i++) begin// lectura de los 10 primeros datos de la memoria ROM
+    #2 address =  i;
+  end
+end
+
+initial begin//test del ALU
+    #60
+    $display("\n");
+    $display(" ALU ");
+    $display("\n");
+    $display(" A        B    | F   |  Y ");
+    $display(" ----------------------------");
+    $monitor(" %b    %b  | %b |  %b ", A, B, F, salida_2);
+
+    A[3] = 0; A[2] = 0; A[1] = 1; A[0] = 0; B[3] = 0; B[2] = 1; B[1] = 0; B[0] = 1;
+    #2  F[2] = 0; F[1] = 0; F[0] = 0;
+    #2  F[2] = 0; F[1] = 0; F[0] = 1;
+    #2  F[2] = 0; F[1] = 1; F[0] = 0;
+    #2  F[2] = 1; F[1] = 0; F[0] = 0;
+    #2  F[2] = 1; F[1] = 0; F[0] = 1;
+    #2  F[2] = 1; F[1] = 1; F[0] = 0;
+    #2  F[2] = 1; F[1] = 1; F[0] = 1;
+    #1  F[2] = 0; F[1] = 0; F[0] = 0;
+    #2  A[3] = 1; A[2] = 0; A[1] = 1; A[0] = 0;
+    #2  F[2] = 1; F[1] = 1; F[0] = 1;
+    #2 $finish;
+  end
+
+  initial begin//ejecutar GTKWave para diagramas de timing.
+    $dumpfile("lab08_tb.vcd");
+    $dumpvars(0, testbench);
+  end
+
+endmodule
